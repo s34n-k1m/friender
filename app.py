b4f732eb6d4dd4896889244053f716065ca0c0e0
Claire Casey <clcasey805@gmail.com>
2021-02-02 11:53:26 -0800

    create generator and seed file

---
+++ app.py
@@ -0,0 +1,25 @@
+import os
+
+from flask import Flask, render_template, request, flash, redirect, session, g, abort
+from flask_debugtoolbar import DebugToolbarExtension
+from sqlalchemy.exc import IntegrityError
+
+# from forms import UserAddForm, UserEditForm, LoginForm, MessageForm
+from models import db, connect_db, User, Like, Dislike
+
+# CURR_USER_KEY = "curr_user"
+
+app = Flask(__name__)
+
+# Get DB_URI from environ variable (useful for production/testing) or,
+# if not set there, use development local db.
+app.config['SQLALCHEMY_DATABASE_URI'] = (
+    os.environ.get('DATABASE_URL', 'postgres:///friender'))
+
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SQLALCHEMY_ECHO'] = False
+app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = True
+app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY', "it's a secret")
+toolbar = DebugToolbarExtension(app)
+
+connect_db(app)
